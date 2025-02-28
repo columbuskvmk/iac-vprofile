5acdf9255de86ac3bf9cca69b690d6a419efe16e
manoj <devops_practice@test.com>
2025-02-28 22:22:33 +0530

    final test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,28 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+variable "access_key" {
+  description = "Access key to AWS console"
 }
-#
-#variable "clusterName" {
-#  description = "Name of the EKS cluster"
-#  type        = string
-#  default     = "vprofileapp-eks"
-#}
+variable "secret_key" {
+  description = "Secret key to AWS console"
+}
 
-#
+variable "table_name" {
+  description = "Dynamodb table name (space is not allowed)"
+  default = "my-first-test-table-state-file"
+}
+
+variable "table_billing_mode" {
+  description = "Controls how you are charged for read and write throughput and how you manage capacity."
+  default = "PAY_PER_REQUEST"
+}
+
+
+variable "environment" {
+  description = "Name of environment"
+  default = "test"
+}
+
+
+variable "account_id" {
+  description = "My Accout Number"
+  default = "account id"
+}
